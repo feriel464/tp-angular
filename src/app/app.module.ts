f29cd174db20206bcc61ab6268e7cb576a2e7b6c
feriel464 <fremadi9@gmail.com>
2022-09-26 19:12:10 +0100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProduitComponent } from './produit/produit.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProduitComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
